50d4eb3ac5eedf6256dc6a00461fb6b205a1baa0
afong <adam.y.w.fong@gmail.com>
2023-10-16 11:18:19 -0700

    Added optionSelect function to server.js

--- server.js
+++ server.js
@@ -15,18 +15,51 @@ const db = mysql.createConnection(
     console.log(`Connected to the database.`)
   );
 
+const  optionSelect = () => {
+  inquirer
+    .prompt({
+      type: 'list',
+      message: 'What would you like to do?',
+      name: 'choice',
+      choices: ['View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Quit']
+    }).then((response) => {
+        switch (response.choice) {
+          case 'View all departments' :
+            viewDepartments();
+            break;
+            case 'View all roles' :
+            viewRoles();
+            break;
+          case 'View all employees' :
+            viewEmployees();
+            break;
+          case 'Add a department' :
+            addDepartment();
+            break;
+          case 'Add a role' :
+            addRole();
+            break;
+          case 'Add an employee' :
+            addEmployee();
+            break;
+          case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+          default:
+            break;
+        }
+    })
+}
+
 function init() {
-    inquirer
-        .prompt(
-            {
-                type: 'list',
-                message: 'Enter up to three characters to include in your logo',
-                name: 'choice',
-                choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
-            }
-        ) .then(
-            
-        )
+  optionSelect();
 }
 
 init();
