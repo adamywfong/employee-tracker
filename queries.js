79398e7f4f08c918be860797a70b2cbd46ff18da
afong <adam.y.w.fong@gmail.com>
2023-10-17 11:17:44 -0700

    Updated viewer.employee to show manager name

--- queries.js
+++ queries.js
@@ -1,7 +1,12 @@
 const viewer = {
-    department: `SELECT * FROM department`,
-    role: `SELECT role.id, title, department.name AS department, salary FROM role JOIN department on role.department_id = department.id`,
-    employee: `SELECT employee.id, first_name, last_name, role.title, department.name AS department, role.salary FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id`
+    department: `SELECT * FROM department'`,
+    role: `SELECT role.id, title, department.name AS department, salary 
+    FROM role JOIN department on role.department_id = department.id'`,
+    employee: `SELECT emp.id,emp.first_name, emp.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee emp
+    JOIN role ON emp.role_id = role.id 
+    JOIN department ON role.department_id = department.id
+    JOIN employee manager ON emp.manager_id = manager.id`
 }
 
 const chooser = {
